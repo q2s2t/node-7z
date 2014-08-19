f330fd7a71fafdcd9bc18fb7d4112a802b62f9a5
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-08-19 11:10:27 +0000

    Add test archive

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 describe('`test` function', function(){
 
   it('should get an error when 7z gets an error', function (done) {
-    sevenzip.test('test/resource/nothere.7z')
+    sevenzip.test('test/nothere.7z')
       .then(null, function (err) {
         expect(err.message.substr(0, 14)).to.eql('Command failed');
         done();
@@ -15,7 +15,7 @@ describe('`test` function', function(){
   });
 
   it('should get a list of files and directories', function (done) {
-    sevenzip.test('test/resource/resource.7z')
+    sevenzip.test('test/zip.7z')
       .then(function (files) {
         expect(files.length).to.be.at.least(6);
         done();
@@ -23,7 +23,7 @@ describe('`test` function', function(){
   });
 
   it('should be usable as a non-promise function', function (done) {
-    sevenzip.testNode('test/resource/resource.7z', function (err, files) {
+    sevenzip.testNode('test/zip.7z', function (err, files) {
       expect(files).to.be.an('array');
       done();
     });
@@ -34,7 +34,7 @@ describe('`test` function', function(){
 describe('`extract` function', function() {
 
   it('should get an error when 7z gets an error', function (done) {
-    sevenzip.extract('test/resource/nothere.7z', '.tmp/extract')
+    sevenzip.extract('test/nothere.7z', '.tmp/extract')
       .then(null, function (err) {
         expect(err.message.substr(0, 14)).to.eql('Command failed');
         done();
@@ -42,7 +42,7 @@ describe('`extract` function', function() {
   });
 
   it('should get a list of files and directories', function (done) {
-    sevenzip.extract('test/resource/resource.7z', '.tmp/extract')
+    sevenzip.extract('test/zip.7z', '.tmp/extract')
       .then(function (files) {
         var t = path.resolve('.tmp/extract', files[0]);
         expect(fs.existsSync(t)).to.eql(true);
@@ -53,7 +53,7 @@ describe('`extract` function', function() {
 
   it('should be usable as a non-promise function', function (done) {
     sevenzip.extractNode(
-      'test/resource/resource.7z',
+      'test/zip.7z',
       '.tmp/extract',
       function (err, files) {
       expect(files).to.be.an('array');
