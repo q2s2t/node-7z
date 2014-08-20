a402cb5e2588b1dee8ef706ea4c29586247108c2
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-08-20 13:31:21 +0000

    Extract with full path working.

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
 // API is consistent with standard use:
 // Promise-style e.g:
 // ```js
-// var seven = require('7z');
+// var seven = new require('7z');
 // seven.test('myArchive.7z')
 //   .then(fulfillHandler, rejectHandler);
 // ```
 // Callback-style e.g:
 // ```js
-// var seven = require('7z');
+// var seven = new require('7z');
 // seven.test('myArchive.7z', function (err, files) {
 //   if (err) errorHandler;
 //   successHandler;
@@ -21,70 +21,93 @@
 var os      = require('os');
 var path    = require('path');
 var process = require('child_process');
+var events  = require('events');
+var util    = require('util');
 var Promise = require('promise');
+
+
+function Api(name) {
+  this.name = (name) ? name : '7z';
+}
+util.inherits(Api, events.EventEmitter);
+
 
 // ## API: test
 // Test integrity of archive.
 //
 // ### Arguments
-//  * `archivePath` The path to the archive you want to analyse.
+//  * `archive` The path to the archive you want to analyse.
 //
 // ### Return values
 //  * `files` A array of all the files *AND* directories in the archives. The
 //    `/` character is used as a path separator on every platform.
 //  * `err` The error as issued by `child_process.exec`.
-var test = function (archivePath) {
+Api.prototype.test = Promise.denodeify(function (archive, callback) {
 
-  archivePath     = path.resolve(archivePath);
-  return new Promise(function (fulfill, reject) {
-    var command = '7z t ' + archivePath;
-    process.exec(command, function (err, stdout) {
-      if (err) return reject(err);
-      var r = [];
-      stdout.split(os.EOL).forEach(function (_line) {
-        if (_line.substr(0, 12) === 'Testing     ') {
-          return r.push(_line.substr(12, _line.length).replace(path.sep, '/'));
-        }
-      });
-      return fulfill(r);
+  archive = path.resolve(archive);
+  var cmd = '7z t ' + archive;
+  process.exec(cmd, function (err, stdout) {
+    if (err) return callback(err, null);
+    var r = [];
+    stdout.split(os.EOL).forEach(function (_line) {
+      if (_line.substr(0, 12) === 'Testing     ') {
+        r.push(_line.substr(12, _line.length).replace(path.sep, '/'));
+      }
     });
+    return callback(null, r);
   });
 
-};
+});
 
-module.exports.test = test;
-module.exports.testNode = Promise.nodeify(test);
 
 // ## API: extract
 // Extract with full paths
 //
 // ### Arguments
-//  * `archivePath` The path to the archive you want to analyse.
-//  * `destinationPath` Where to extract the archive.
+//  * `archive` The path to the archive you want to analyse.
+//  * `dest` Where to extract the archive.
 //
 // ### Return values
 //  * `files` A array of all the extracted files *AND* directories. The `/`
 //    character is used as a path separator on every platform.
 //  * `err` The error as issued by `child_process.exec`.
-var extract = function (archivePath, destinationPath) {
+//
+// ### Events
+//  * `data` Emitted when files are extracted. Has one parameter `(files)`, an
+//    array of files and directories processed.
+Api.prototype.extract = Promise.denodeify(function (archive, dest, callback) {
 
-  archivePath     = path.resolve(archivePath);
-  destinationPath = path.resolve(destinationPath);
-  return new Promise(function (fulfill, reject) {
-    var command = '7z x ' + archivePath + ' -y -o' + destinationPath;
-    process.exec(command, function (err, stdout) {
-      if (err) return reject(err);
-      var r = [];
-      stdout.split(os.EOL).forEach(function (_line) {
-        if (_line.substr(0, 12) === 'Extracting  ') {
-          return r.push(_line.substr(12, _line.length).replace(path.sep, '/'));
-        }
-      });
-      return fulfill(r);
+  archive  = path.resolve(archive);
+  dest     = path.resolve(dest);
+  var self = this;
+  var err  = null;
+  var cmd  = ('x ' + archive + ' -y -o' + dest).split(' ');
+  var run  = process.spawn('7z', cmd, {
+    detached: true,
+    stdio: 'pipe'
+  });
+  run.on('close', function (code) {
+    if (err) {
+      return callback(err);
+    }
+    return callback(null);
+  });
+  run.stdout.on('data', function (data) {
+    var files = [];
+    var res   = /Error:\n(.*)/g.exec(data.toString());
+    if (res) {
+      err = new Error(res[1]);
+    }
+    data.toString().split(os.EOL).forEach(function (_line) {
+      if (_line.substr(0, 12) === 'Extracting  ') {
+        files.push(_line.substr(12, _line.length).replace(path.sep, '/'));
+      }
     });
+    self.emit('data', files);
   });
 
-};
+});
 
-module.exports.extract = extract;
-module.exports.extractNode = Promise.nodeify(extract);
+
+// Exports the Api object.
+module.exports = Api;
