999ba8b879aa1a3a9383abca1cd3a07040f53d99
Quentin Rossetti <quentin.rossetti@gmail.com>
2014-08-25 09:52:32 +0000

    Extract (flat) added.

---
+++ test/lib/extract.js
@@ -0,0 +1,46 @@
+/*global describe, it, afterEach */
+var expect = require('chai').expect;
+var fs = require('fs');
+var rimraf = require('rimraf');
+var extract = require('../../lib/extract');
+
+describe('Module: `extract`', function () {
+  
+  afterEach(function () { rimraf.sync('.tmp/test'); });
+  
+  it('should return an error on 7z error', function (done) {
+    extract('test/nothere.7z', '.tmp/test')
+    .catch(function (err) {
+      expect(err).to.be.an.instanceof(Error);
+      done();
+    });
+  });
+
+  it('should return an error on output duplticate', function (done) {
+    extract('test/zip.7z', '.tmp/test', { o: '.tmp/test/duplicate' })
+    .catch(function (err) {
+      expect(err).to.be.an.instanceof(Error);
+      done();
+    });
+  });
+  
+  it('should return entries on progress', function (done) {
+    extract('test/zip.7z', '.tmp/test')
+    .progress(function (entries) {
+      expect(entries.length).to.be.at.least(1);
+      done();
+    });
+  });
+  
+  it('should extract on the right path', function (done) {
+    extract('test/zip.7z', '.tmp/test')
+    .then(function () {
+      expect(fs.existsSync('.tmp/test/file0.txt')).to.be.eql(true);
+      expect(fs.existsSync('.tmp/test/file1.txt')).to.be.eql(true);
+      expect(fs.existsSync('.tmp/test/file2.txt')).to.be.eql(true);
+      expect(fs.existsSync('.tmp/test/file3.txt')).to.be.eql(true);
+      done();
+    });
+  });
+  
+});
