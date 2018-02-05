6a34e01468d535b5e8b9c0251ff4385c7658be56
techno-express <technoexpressnet@gmail.com>
2018-02-05 14:05:07 -0500

    testing corrections

--- test/util/path.test.js
+++ test/util/path.test.js
@@ -4,9 +4,10 @@ var expect   = require('chai').expect;
 var exec = require('child_process').execSync;
 var path = require('../../util/path');
 
-describe('Utility: `path`', function () {   
-  it('should return deflaut flags with no args', function () {
-    var pathInSystem = exec('which 7za').toString();
+describe('Utility: `path`', function () {
+  it('should return deflaut flags with no args', function () {   
+    var _7zcmd = path();
+    var pathInSystem = exec('which ' + _7zcmd).toString();
   });
 	
 });
