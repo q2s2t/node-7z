0408b5071a93b3658ce2d18112f0e8ba563f0cb7
techno-express <technoexpressnet@gmail.com>
2018-02-05 12:49:17 -0500

    testing

--- util/path.js
+++ util/path.js
@@ -2,20 +2,21 @@
 var path  = require('path');
 
 module.exports = function (options) {
-  
-  // Create a string that can be parsed by `run`.
-  try {
     
+    var macversion = (process.platform == "darwin") ? require('macos-release').version : '';
+    var binarypath = path.join(__dirname, '..','binaries', (macversion=='') ? process.platform : process.platform, macversion);
+    
+  // Create a string that can be parsed by `run`.
+  try {    
+      
     if (options.path) {
       return options.path;
-    } else {    
-      var macversion = (process.platform == "darwin") ? require('macos-release').version : '';
-      var binarypath = path.join(__dirname, '..','binaries', (macversion=='') ? process.platform : process.platform, macversion);
+    } else {
       return path.join(binarypath, process.platform === "win32" ? '7za.exe' : '7za');
     }
     
   } catch (e) {
-    throw new Error('Path to the 7-Zip bin not found');
+    return path.join(binarypath, process.platform === "win32" ? '7za.exe' : '7za');
   }
   
 };
