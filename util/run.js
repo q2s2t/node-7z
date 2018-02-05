bab38c6b0d66f6cc77be4a9374386e4d5190c291
techno-express <technoexpressnet@gmail.com>
2018-02-05 13:28:04 -0500

    testing, removed option to pass path since binaries are include instead add path to command at 7za run

--- util/run.js
+++ util/run.js
@@ -15,13 +15,6 @@ var utilSwitches = require('./switches');
  */
 module.exports = function (command, switches) {   
   return when.promise(function (fulfill, reject, progress) {
-    // add platform binary to environment path
-    var envPath = process.env.path;
-    if (envPath.indexOf('7za') < 0) {
-        var macos = (process.platform == "darwin") ? require('macos-release').version : '';
-        var pathto7z = path.join(__dirname, "..","binaries", macos == '' ? process.platform : process.platform, macos );
-        process.env.path += (envPath[envPath.length -1] === ';') ? pathto7z : ';' + pathto7z;
-    }
 
     // Parse the command variable. If the command is not a string reject the
     // Promise. Otherwise transform the command into two variables: the command
@@ -29,7 +22,10 @@ module.exports = function (command, switches) {
     if (typeof command !== 'string') {
       return reject(new Error('Command must be a string'));
     }
-    var cmd  = command.split(' ')[0];
+    var macos = (process.platform == "darwin") ? require('macos-release').version : '';
+    var pathto7z = path.join(__dirname, "..","binaries", macos == '' ? process.platform : process.platform, macos );   
+    // add platform binary to command
+    var cmd  = path.join(pathto7z,command.split(' ')[0]);
     var args = [ command.split(' ')[1] ];
 
     // Parse and add command (non-switches parameters) to `args`.
