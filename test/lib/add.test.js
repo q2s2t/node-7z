ed32e400c439805e89dfd4c486b66faff6906e8d
techno-express <technoexpressnet@gmail.com>
2018-02-05 22:22:47 -0500

    revert

--- test/lib/add.test.js
+++ test/lib/add.test.js
@@ -8,9 +8,17 @@ var _7zcmd = path();
 
 describe('Method: `Zip.add`', function() {
 
-  it('should return entries on progress', function(done) {
+  it('should return an error on 7z error', function (done) {
+    add('.tmp/test/addnot.7z', '.tmp/test/nothere', { '???': true })
+    .catch(function (err) {
+      expect(err).to.be.an.instanceof(Error);
+      done();
+    });
+  });
+
+  it('should return entries on progress', function (done) {
     add('.tmp/test/add.zip', '*.md')
-    .progress(function(entries) {
+    .progress(function (entries) {
       expect(entries.length).to.be.at.least(1);
       done();
     })
@@ -19,16 +27,26 @@ describe('Method: `Zip.add`', function() {
     });
   });
 
-  it('should accept array as source', function(done) {
+  it('should accept array as source', function (done) {
     var store = [];
     add('.tmp/test/add.zip', ['*.md', '*.js'])
-    .progress(function(entries) {
+    .progress(function (entries) {
       entries.forEach(function (e) {
         store.push(e);
       });
     })
-    .done(function() {
+    .done(function () {
       expect(store.length).to.be.at.least(4);
+      done();
+    });
+  });
+
+  it('should accept a path', function (done) {
+    add('.tmp/test/add.zip', '*.md', {
+      path: _7zcmd
+    })
+    .progress(function (entries) {
+      expect(entries.length).to.be.at.least(1);
       done();
     });
   });
