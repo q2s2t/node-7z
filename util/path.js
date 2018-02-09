7a26e3d8e65c96e957c61fc4cf86bee1befe0a30
techno-express <technoexpressnet@gmail.com>
2018-02-08 20:27:18 -0500

    simplified code

--- util/path.js
+++ util/path.js
@@ -1,21 +1,16 @@
 'use strict';
-var path  = require('path');
 
 module.exports = function (options) {
-    
-    var macversion = (process.platform == "darwin") ? require('macos-release').version : '';
-    var binarypath = path.resolve(__dirname, '..','binaries', (macversion=='') ? process.platform : process.platform, macversion);
-    
+        
   // Create a string that can be parsed by `run`.
   try {    
       
-    if (options.path) {
-      return options.path;
-    } else {
-      return path.join(binarypath, process.platform === "win32" ? '7za.exe' : '7za');
-    }
+    if (options.path) return options.path;
     
   } catch (e) {
+    var path = require('path');
+    var macversion = (process.platform == "darwin") ? require('macos-release').version : '';
+    var binarypath = path.resolve(__dirname, '..','binaries', (macversion=='') ? process.platform : process.platform, macversion);
     return path.join(binarypath, process.platform === "win32" ? '7za.exe' : '7za');
   }
   
