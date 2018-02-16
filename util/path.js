7883e853ec2eefd59a3c858b52504d67aa9ebc90
Lawrence <technoexpressnet@gmail.com>
2018-02-16 17:12:27 -0500

    test fix errors for binary/path method

--- util/path.js
+++ util/path.js
@@ -2,18 +2,15 @@
 var path = require("path");
 	
 module.exports = function (options) {
-
-  // Create a string that can be parsed by `run`.
-	if (options.hasOwnProperty(path)) return options.path;     
-	else {
-		try { var macosversion = (process.platform == "darwin") ? require('macos-release').version : ''; }
-		catch (e) { var macosversion = 'undefined' }
-		
+	try { var macosversion = (process.platform == "darwin") ? require('macos-release').version : ''; }
+	catch (e) { var macosversion = 'undefined' }
+	
+	if ((options) && (options typeof 'object' ) && (options.hasOwnProperty('path'))) return options.path;  
+	else {		
 		var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform + path.sep + macosversion );
 		var binaryfilename = (process.platform == "win32") ? '7za.exe' : '7za'; 
 		return { path: binarypath, 
 			filename: binaryfilename, 
 			fullpath: path.join(binarypath, binaryfilename) }
-	}
-  
+	}  
 };
