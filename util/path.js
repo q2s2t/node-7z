4ff188e3d23145b9929ce1d6af2b7803e3e02e69
Lawrence <technoexpressnet@gmail.com>
2018-02-16 16:38:06 -0500

    refactor binary/path method issues when testing under mac os platform

--- util/path.js
+++ util/path.js
@@ -2,32 +2,18 @@
 var path = require("path");
 	
 module.exports = function (options) {
-    
+
   // Create a string that can be parsed by `run`.
-  try {    
-      
-    if (options.path) return options.path;
+	if (options.hasOwnProperty(path)) return options.path;     
 	else {
-		
 		try { var macosversion = (process.platform == "darwin") ? require('macos-release').version : ''; }
-		catch (e) { var macosversion = ''; }
+		catch (e) { var macosversion = 'undefined' }
 		
-		var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform, macosversion );
-		var binaryfilename = (process.platform == "win32") ? '7za.exe' : '7za';
+		var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform + path.sep + macosversion );
+		var binaryfilename = (process.platform == "win32") ? '7za.exe' : '7za'; 
 		return { path: binarypath, 
 			filename: binaryfilename, 
 			fullpath: path.join(binarypath, binaryfilename) }
-	}    
-  } catch (e) {
-	  
-	try { var macosversion = (process.platform == "darwin") ? require('macos-release').version : ''; }
-	catch (e) { var macosversion = ''; }
-	
-    var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform, macosversion );
-    var binaryfilename = (process.platform == "win32") ? '7za.exe' : '7za';
-    return { path: binarypath, 
-		filename: binaryfilename, 
-		fullpath: path.join(binarypath, binaryfilename) }
-  }
+	}
   
 };
