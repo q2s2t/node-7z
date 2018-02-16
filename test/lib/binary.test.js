16011c0fa7172a82407ad4c7d3112cdd7b0aac66
Lawrence <technoexpressnet@gmail.com>
2018-02-16 15:30:37 -0500

    update binary/path test for mac os

--- test/lib/binary.test.js
+++ test/lib/binary.test.js
@@ -33,3 +33,45 @@ describe('Method: `Zip.binary`', function () {
         done();
     });		
 });
+
+
+
+describe('Method: `Zip.binary` with platform set to `darwin`', function () {
+	    // save original process.platform    
+    before(function() { this.originalPlatform = Object.getOwnPropertyDescriptor(process, 'platform');
+    // redefine process.platform
+        Object.defineProperty(process, 'platform', { value: 'darwin' }); });
+    // restore original process.platform
+    after(function() { Object.defineProperty(process, 'platform', this.originalPlatform); });
+	
+    var _7zcmd = binary();
+    
+    it('should return an object', function (done) {
+        expect(_7zcmd).to.be.an('object');
+        done();
+    });      
+   
+    it('should return an object with key fields of `path, filename, fullpath` and be string', function (done) {
+		expect(_7zcmd).to.have.property('path');
+		expect(_7zcmd).to.have.property('filename');
+		expect(_7zcmd).to.have.property('fullpath');
+		expect(_7zcmd.fullpath).to.be.a('string');
+        done();
+    });  
+  
+    it('should return an string from `object` with correct key { path: 7za } ', function (done) {
+        var _7zpath = binary({ path: '7za' });
+        expect(_7zpath).to.be.a('string');
+        done();
+    });
+	
+    it('should return an object from `object` with wrong key or no key', function (done) {
+        var _7zpathwrong = binary({ nopath: '7za' });
+        expect(_7zpathwrong).to.be.an('object');
+        var _7zpathno = binary('???');
+        expect(_7zpathno).to.be.an('object');
+        done();
+    });		
+});
+
+
