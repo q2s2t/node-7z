71fd0daa34908cafd67a70ad195c1205a34bb801
techno-express <technoexpressnet@gmail.com>
2018-08-26 00:17:42 -0400

    test commit, there seems to be bugs in original source, build halts on overwrite of my commit from original

--- util/path.js
+++ util/path.js
@@ -10,7 +10,7 @@ module.exports = function (options) {
     
   } catch (e) {
     var macosversion = (process.platform == "darwin") ? require('macos-release').version : '';
-    var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform, macosversion );
+    var binarypath = path.join(__dirname, "..","binaries", (macosversion == '') ? process.platform : process.platform + path.sep + macosversion );
     var binaryfilename = (process.platform == "win32") ? '7za.exe' : '7za';
     return { path: binarypath, filename: binaryfilename, fullpath: path.join(binarypath, binaryfilename) }
   }
