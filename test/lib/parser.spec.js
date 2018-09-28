32655611d9e2775869a288ebfb3c9bb1e1fd7dcf
Quentin Rossetti <quentin.rossetti@gmail.com>
2018-09-28 21:23:09 +0200

    feat: Parse lines with colon-separated value

--- test/lib/parser.spec.js
+++ test/lib/parser.spec.js
@@ -1,14 +1,14 @@
 /* global describe, it */
 import { expect } from 'chai'
-import { matchProgress, matchPropsColon, matchPropsColonDuo, matchPropsEquals, matchSymbolFile } from '../../lib/parser.js'
+import { matchProgress, matchPropsColon, matchPropsEquals, matchSymbolFile } from '../../lib/parser.js'
 
 describe('Specification: parser.js', function () {
-  it('progress should return null on non match', function () {
+  it('matchProgress() should return null on non match', function () {
     const r = matchProgress('+ test/file/null')
     expect(r).to.be.null
   })
 
-  it('progress should return progress info as int', function () {
+  it('matchProgress() should return progress info as int', function () {
     const digit1File = matchProgress('  1% 3')
     expect(digit1File.percent).to.equal(1)
     expect(digit1File.fileCount).to.equal(3)
@@ -29,5 +29,36 @@ describe('Specification: parser.js', function () {
 
     const digit3NoFile = matchProgress('100%')
     expect(digit3NoFile.percent).to.equal(100)
+  })
+
+  it('matchPropsColon() should return null on non match', function () {
+    const r = matchPropsColon('+ test/file/null')
+    expect(r).to.be.null
+  })
+
+  it('matchPropsColon() should return props and values', function () {
+    const basic = matchPropsColon('Prop: Data')
+    expect(basic).to.be.an('array')
+    expect(basic).to.have.lengthOf(1)
+    expect(basic[0]['Prop']).to.equal('Data')
+
+    const space = matchPropsColon('Prop of archive: 322 MB')
+    expect(space).to.be.an('array')
+    expect(space).to.have.lengthOf(1)
+    expect(space[0]['Prop of archive']).to.equal('322 MB')
+  })
+
+  it('matchPropsColon() should works with line containing 2 infos', function () {
+    const basic = matchPropsColon('Prop1: Data1,  # Prop2: Data2')
+    expect(basic).to.be.an('array')
+    expect(basic).to.have.lengthOf(2)
+    expect(basic[0]['Prop1']).to.equal('Data1')
+    expect(basic[1]['Prop2']).to.equal('Data2')
+
+    const space = matchPropsColon('Prop 1: Data 1,  # Prop 2: Data 2')
+    expect(space).to.be.an('array')
+    expect(space).to.have.lengthOf(2)
+    expect(space[0]['Prop 1']).to.equal('Data 1')
+    expect(space[1]['Prop 2']).to.equal('Data 2')
   })
 })
