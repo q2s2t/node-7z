80673cadf707e26e72852474fbea71224f97b4da
Quentin Rossetti <quentin.rossetti@gmail.com>
2018-10-11 23:16:00 +0200

    refactor: Unify matchProps() to capture equal and colon separated-values

--- test/unit/parser.spec.js
+++ test/unit/parser.spec.js
@@ -1,6 +1,6 @@
 /* global describe, it */
 import { expect } from 'chai'
-import { matchProgress, matchPropsColon, matchPropsEquals } from '../../lib/parser.js'
+import { matchProgress, matchProps } from '../../lib/parser.js'
 
 describe('Specification: parser.js', function () {
   it('matchProgress() should return undefined on non match', function () {
@@ -96,51 +96,65 @@ describe('Specification: parser.js', function () {
     expect(r3.file).to.equal('DirImages/LICENSE')
   })
 
-  it('matchPropsColon() should return null on non match', function () {
-    const r = matchPropsColon('+ test/file/null')
+  it('matchProps() should return null on non match', function () {
+    const r = matchProps('+ test/file/null')
     expect(r).to.be.null
   })
 
-  it('matchPropsColon() should return props and values', function () {
-    const basic = matchPropsColon('Prop: Data')
+  it('matchProps() should return props and values', function () {
+    const basic = matchProps('Prop: Data')
     expect(basic).to.be.an('array')
     expect(basic).to.have.lengthOf(1)
     expect(basic[0]['Prop']).to.equal('Data')
 
-    const space = matchPropsColon('Prop of archive: 322 MB')
+    const space = matchProps('Prop of archive: 322 MB')
     expect(space).to.be.an('array')
     expect(space).to.have.lengthOf(1)
     expect(space[0]['Prop of archive']).to.equal('322 MB')
   })
 
-  it('matchPropsColon() should works with line containing 2 infos', function () {
-    const basic = matchPropsColon('Prop1: Data1,  # Prop2: Data2')
+  it('matchProps() should works with line containing 2 infos', function () {
+    const basic = matchProps('Prop1: Data1,  # Prop2: Data2')
     expect(basic).to.be.an('array')
     expect(basic).to.have.lengthOf(2)
     expect(basic[0]['Prop1']).to.equal('Data1')
     expect(basic[1]['Prop2']).to.equal('Data2')
 
-    const space = matchPropsColon('Prop 1: Data 1,  # Prop 2: Data 2')
+    const space = matchProps('Prop 1: Data 1,  # Prop 2: Data 2')
     expect(space).to.be.an('array')
     expect(space).to.have.lengthOf(2)
     expect(space[0]['Prop 1']).to.equal('Data 1')
     expect(space[1]['Prop 2']).to.equal('Data 2')
   })
 
-  it('matchPropsEquals() should return null on non match', function () {
-    const r = matchPropsEquals('+ test/file/null')
+  it('matchProps() should return null on non match', function () {
+    const r = matchProps('+ test/file/null')
     expect(r).to.be.null
   })
 
-  it('matchPropsEquals() should return props and values', function () {
-    const basic = matchPropsEquals('Prop = Data')
+  it('matchProps() should return props and values', function () {
+    const basic = matchProps('Prop = Data')
     expect(basic).to.be.an('array')
     expect(basic).to.have.lengthOf(1)
     expect(basic[0]['Prop']).to.equal('Data')
 
-    const space = matchPropsEquals('Prop of archive = 322 MB')
+    const space = matchProps('Prop of archive = 322 MB')
     expect(space).to.be.an('array')
     expect(space).to.have.lengthOf(1)
     expect(space[0]['Prop of archive']).to.equal('322 MB')
+  })
+
+  it('matchProps() should get props of values with colon', function () {
+    const equalFirst = matchProps('Equal first = True but:this = is valid val')
+    expect(equalFirst).to.be.an('array')
+    expect(equalFirst).to.have.lengthOf(1)
+    expect(equalFirst[0]['Equal first']).to.equal('True but:this = is valid val')
+  })
+
+  it('matchProps() should get props of values with equal', function () {
+    const colonFirst = matchProps('Colon first: True but:this = is valid val')
+    expect(colonFirst).to.be.an('array')
+    expect(colonFirst).to.have.lengthOf(1)
+    expect(colonFirst[0]['Colon first']).to.equal('True but:this = is valid val')
   })
 })
