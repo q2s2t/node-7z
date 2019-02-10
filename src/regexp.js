b0a7d778a7941c08293180a8f6ec6ab2bf7be65a
Quentin Rossetti <quentin.rossetti@gmail.com>
2019-02-10 20:44:54 +0100

    chore: Use named capture groups polyfill on Node.js < v10

--- src/regexp.js
+++ src/regexp.js
@@ -1,11 +1,22 @@
-const LINE_SPLIT = /\n|\r\n|\x08+|\r +\r/
-const BODY_PROGRESS = /^ *(?<percent>\d+)% ?(?<fileCount>\d+)? ?(?<file>.*)$/
-const BODY_SYMBOL_FILE = /^(?<symbol>[=TU+R.-]) (?<file>.+)$/
-const BODY_HASH = /^(?<hash>\S+)? +(?<size>\d*) +(?<file>.+)$/
-const END_OF_STAGE_HYPHEN = /^(-+ +)+-+$/
-const INFOS = /^(?<property>.+?)(?<separator>( = )|(: +))(?<value>.+)$/
-const INFOS_SPLIT = /, +# /
-const ERROR = /(?<level>WARNING|ERROR): (?<message>.*)(\r\n)?(\n)?/i
+// @COMPAT Named capture groups aren't available before Node.js 10. This
+// compatibility checks the running Node.js version and apply a polyfill
+// if needed.
+// Support until Jan 2020 (https://github.com/nodejs/Release)
+const semver = /v(\d+)\.(\d+)\.(\d+)/
+const nodeVersionResults = process.version.match(semver)
+const nodeVersionMajor = parseInt(nodeVersionResults[1])
+if (nodeVersionMajor < 10) {
+  require('regexp-polyfill')
+}
+
+const LINE_SPLIT = new RegExp('\n|\r\n|\x08+|\r +\r')
+const BODY_PROGRESS = new RegExp('^ *(?<percent>\\d+)% ?(?<fileCount>\\d+)? ?(?<file>.*)$')
+const BODY_SYMBOL_FILE = new RegExp('^(?<symbol>[=TU+R.-]) (?<file>.+)$')
+const BODY_HASH = new RegExp('^(?<hash>\\S+)? +(?<size>\\d*) +(?<file>.+)$')
+const END_OF_STAGE_HYPHEN = new RegExp('^(-+ +)+-+$')
+const INFOS = new RegExp('^(?<property>.+?)(?<separator>( = )|(: +))(?<value>.+)$')
+const INFOS_SPLIT = new RegExp(', +# ')
+const ERROR = new RegExp('(?<level>WARNING|ERROR): (?<message>.*)(\r\n)?(\n)?', 'i')
 
 module.exports = {
   LINE_SPLIT,
