708a5fdf0c5b697eb8f00cc27fe0d5f8d74364ab
Quentin Rossetti <quentin.rossetti@gmail.com>
2019-10-27 20:51:41 +0100

    chore: Update dependencies
    BREAKING CHANGES: Drop Node.JS 8 support.

--- src/regexp.js
+++ src/regexp.js
@@ -1,14 +1,3 @@
-// @COMPAT Named capture groups aren't available before Node.js 10.
-// Support until Jan 2020 (https://github.com/nodejs/Release)
-const RegExp = (() => {
-  try {
-    new RegExp('(?<test>a)') // eslint-disable-line no-new
-    return RegExp
-  } catch (error) {
-    return require('named-regexp-groups')
-  }
-})()
-
 const LINE_SPLIT = new RegExp('\n|\r\n|\x08+|\r +\r')
 const BODY_PROGRESS = new RegExp('^ *(?<percent>\\d+)% ?(?<fileCount>\\d+)? ?(?<file>.*)$')
 const BODY_SYMBOL_FILE = new RegExp('^(?<symbol>[=TU+R.-]) (?<file>.+)$')
