294ee478d25511e821da30884d32aa32e1fafd65
Bruno J. Spolavori <bspol.323@gmail.com>
2020-06-04 20:27:30 -0300

    Typescipt compatibility
    export module from typescript intellisense

---
+++ src/main.d.ts
@@ -0,0 +1,13 @@
+export const add: () => any
+// export const delete: () => any // Variable declaration expected
+export const extract: () => any
+export const extractFull: () => any
+export const hash: () => any
+export const list: () => any
+export const rename: () => any
+export const test: () => any
+export const update: () => any
+export const listenFactory: () => any
+export const listen: () => any
+export const mainFactory: () => any
+export const main: () => any
